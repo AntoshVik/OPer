c03e5a6d3874ebff57a35c7b18378427f5d14b49
Anton <jereeko5@gmail.com>
2026-02-26 10:28:50 +0300

    Delete ConfigLoader.h

--- ConfigLoader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Algorithm.h"
-#include <vector>
-#include <string>
-
-class ConfigLoader {
-public:
-    static std::vector<Algorithm> loadFromDirectory(const std::string& dirPath);
-};
